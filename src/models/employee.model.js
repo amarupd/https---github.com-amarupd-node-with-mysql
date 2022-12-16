0498ce711b529e407c002dce06ff147f8bfd0d69
amarupd <amarduttupadhyay@gmail.com>
2022-12-16 13:07:36 +0530

    comn

---
+++ src/models/employee.model.js
@@ -0,0 +1,27 @@
+var dbConn=require("../../config/db.config");
+var Employee=(employee)=>{
+    this.first_name=employee.first_name;
+    this.last_name=employee.last_name;
+    this.email=employee.email;
+    this.phone=employee.phone;
+    this.organisation=employee.organisation;
+    this.designation=employee.designation;
+    this.salary=employee.salary;
+    this.status=employee.status ? employee.status : 1;
+    this.created_at=new Date();
+    this.updated_at=new Date();
+}
+
+
+//get all the employee
+
+Employee.getAllEmployee=(res)=>{
+    dbConn.query('SELECT*FROM EMPLOYEES',(err,data)=>{
+        if(err) {
+            console.log("error fetching the data from sql",err);
+            result(null,err);
+        }
+        console.log("employees details fetched succesfully");
+        result(null,res);
+    })
+}
